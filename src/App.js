8f29760baaffe7d1185cca78a205f2734ca6f596
xodud3008 <xodud3008@gmail.com>
2021-12-06 15:18:43 +0900

    [Post] Finish ReactJs Theory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
+function Hello(){
+  useEffect(() => {
+    console.log("hi :)")
+    return () => console.log("bye :(")    
+  }, []);
+  return <h1>Hello</h1>
+}
 
 function App() {
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing((prev) => !prev)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
